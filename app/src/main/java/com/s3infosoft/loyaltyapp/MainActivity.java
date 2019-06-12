f3af489b523290af51cc5b6b8d3f520eb79eb1ce
dev-sourabh <sharmasourabh121.ss@gmail.com>
2019-06-12 17:41:32 +0530

    Login Layout

--- app/src/main/java/com/s3infosoft/loyaltyapp/MainActivity.java
+++ app/src/main/java/com/s3infosoft/loyaltyapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.s3infosoft.loyaltyapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
