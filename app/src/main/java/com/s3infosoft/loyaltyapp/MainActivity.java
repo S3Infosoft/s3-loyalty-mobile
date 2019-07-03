64ec61c07fda43b320f697d76170cd84b517f8bc
Varun-L <varun.001raone@gmail.com>
2019-07-03 15:10:07 +0530

    test

--- app/src/main/java/com/s3infosoft/loyaltyapp/MainActivity.java
+++ app/src/main/java/com/s3infosoft/loyaltyapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String st;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
